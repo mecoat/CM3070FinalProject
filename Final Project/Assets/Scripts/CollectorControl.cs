5c5004cadebabf919c33db5498bc2bc659f70aae
mecoat <66117150+mecoat@users.noreply.github.com>
2024-11-22 14:10:27 +0000

    basic collector movement added

---
+++ Final Project/Assets/Scripts/CollectorControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectorControl : MonoBehaviour
+{
+
+    [SerializeField]
+    float movementSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movementZ = Input.GetAxis("Vertical");
+        float movementX = Input.GetAxis("Horizontal");
+
+        transform.Translate(new Vector3(movementX, 0, movementZ) * movementSpeed * Time.deltaTime);
+    }
+}
