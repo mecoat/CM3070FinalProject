aa7d742f3f951068e3841bf590d12bd6416a2279
mecoat <66117150+mecoat@users.noreply.github.com>
2024-11-25 17:54:27 +0000

    Commenting completed on current content

--- Final Project/Assets/Scripts/CollectorControl.cs
+++ Final Project/Assets/Scripts/CollectorControl.cs
@@ -65,91 +65,129 @@ public class CollectorControl : MonoBehaviour
             rb.MovePosition(transform.position + newPos);
         }
 
+        //if the player presses Fire2 button (Left Alt)
         if (Input.GetButtonDown("Fire2"))
         {
+            //chnge playerMove to false (which stops player control)
             playerMove = false;
 
+            //trigger the dropCollector function
             dropCollector();
         }
 
+        //if the playerMove variable of false (plaer control is off)
         if (!playerMove)
         {
+            // if variable isDropping is true (collecor is dropping)
             if (isDropping)
             {
+                //create a vector with a negative y value (downwards)
                 Vector3 dropPos = new Vector3(0, -5, 0) * movementSpeed * Time.deltaTime;
+                //adjust the position of the collector RigidBody by adding the new variable to current location vector
                 rb.MovePosition(transform.position + dropPos);
             } 
+            //otherwise, if either hasCollected (Collector touched an Object) or isn't dropping (touched the floor)
             else if (hasCollected || !isDropping)
             {
+                //create a vector with a positive y value (upwards)
                 Vector3 raisePos = new Vector3(0, 3, 0) * movementSpeed * Time.deltaTime;
+                //adjust the position of the collector RigidBody by adding the new variable to current location vector
                 rb.MovePosition(transform.position + raisePos);
 
+                //if the rigib body y position  is greater than or equal to 9 (home height)
                 if (rb.transform.position.y >= 9)
                 {
+                    //prevent the Reigid body from moving more in y direction
                     rb.constraints = RigidbodyConstraints.FreezePositionY;
 
+                    //if it hasn't collected
                     if (!hasCollected)
                     {
+                        //change playerMove to true (return control to player)
                         playerMove = true;
+                        //output to console to show what's happening)
                         Debug.Log("return control to player");
                     }
+                    //otherwise if hasDeposited is false (there is an object on the collector)
                     else if (!hasDeposited)
                     {
+                        //if the position is not at the try yet
                         if (rb.transform.position.x <= trayLoc.x  && rb.transform.position.z <= trayLoc.z)
                         {
+                            //output to console to show what's happening)
                             Debug.Log("taking object to tray");
+                            //move collector towards the tray
                             rb.MovePosition(transform.position + trayPos);
-
                         }
+                        //otherwise ... (it's reached the tray)
                         else
                         {
+                            //change has depostied to true (as objet will be put into try
                             hasDeposited = true;
+                            //more to add here to actually deposit the object
+
+                            //output to console to show what's happening)
                             Debug.Log("returning to arena");
-
+                            //move away from the tray towards earlier position
                             rb.MovePosition(transform.position - trayPos);
-
                         }
-
-
                     }
+                    //otherwise... (hasDeposited is true)
                     else
                     {
+                        //change playerMove to true (return control to player)
                         playerMove = true;
+                        //output to console to show what's happening)
                         Debug.Log("return control to player");
+                        //change hasDeposited to false (revert to default
                         hasDeposited = false;
+                        //change has collected to false (refert to default)
                         hasCollected = false;
-
-
                     }
                 }
             }
         }
     }
 
+    //function to drop the collector
     private void dropCollector()
     {
+        //remove all constraints on the rigid body
         rb.constraints = RigidbodyConstraints.None;
+        //place a contrasint to prevent rotation
         rb.constraints = RigidbodyConstraints.FreezeRotation;
 
+        //output to console to show what's happening)
         Debug.Log("Dropping collector");
+        //chage isDroppping variable to true to indicate that the collector is dropping
         isDropping = true;
     }
 
+    //when a collision occurs
     private void OnCollisionEnter(Collision collision)
     {
+        //output the tag of the element collided with to the console
         Debug.Log(collision.gameObject.tag);
 
+        //if the collision with with something that has the tag of "Object"
         if (collision.gameObject.tag == "Object")
         {
+            //change hasCollected to true (indicates that the object is collected)
             hasCollected = true;
+            //More to add here to actually collect the object
+
+            //change isdropping to false (we do not want to the object to continue down)
             isDropping = false;
+            //output to console to show what's happening)
             Debug.Log("collecting object");
         }
+        //otherwise, if the collector isDropping (ie not moving sideways and colliding with walls...
         else if (isDropping)
         {
+            //change isdropping to false (we do not want to the object to continue down)
             isDropping = false;
+            //output to console to show what's happening)
             Debug.Log("Dropped, no object");
         }
-
     }
 }
