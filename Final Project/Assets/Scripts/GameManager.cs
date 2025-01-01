047d13a11bd48c7e63c7f59f013fac0de9c221d3
mecoat <66117150+mecoat@users.noreply.github.com>
2025-01-01 12:16:58 +0000

    adding levels and timer script

---
+++ Final Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    [Header("Level Controls")]
+    private int currScore;
+    public int levelNo;
+
+    private GameObject HUD;
+    private GameObject displayScore;
+    [SerializeField]
+    Levels currentLevel;
+
+    private int timeForLevel;
+
+    [SerializeField] public CustomTimer inGameTimer;
+
+
+    private void Awake()
+    {
+        GetLevelData();
+
+        //objectList = new List<GameObject>();
+    }
+
+
+    /// <summary>
+    /// Gets the data of the current level through playerPrefs and finding assets
+    /// </summary>
+    private void GetLevelData()
+    {
+        int levelNum = PlayerPrefs.GetInt("LevelNum");
+        string levelName = "Level" + levelNum + "Data";
+        //https://docs.unity3d.com/ScriptReference/AssetDatabase.FindAssets.html
+        //string[] guids = AssetDatabase.FindAssets(levelName);
+
+        //https://docs.unity3d.com/2020.1/Documentation/ScriptReference/Resources.Load.html accessed 31/12/24
+        var levelToLoad = Resources.Load<Levels>("Level Data/" + levelName);
+
+        currentLevel = levelToLoad;
+        levelNo = levelNum;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitializeLevelData();
+
+        if (timeForLevel > 0)
+        {
+            
+            //Activate the timer related objects
+            inGameTimer.gameObject.SetActive(true);
+            inGameTimer.SetTimeFromSeconds(timeForLevel);
+            inGameTimer.StartTimer();
+            StartCoroutine(WaitForTimerRunOut());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Initalize the data from the current levels data into variables
+    /// </summary>
+    private void InitializeLevelData()
+    {
+        timeForLevel = currentLevel.timer;
+
+    }
+
+
+    /// <summary>
+    /// Generate the objects for the current level
+    /// based on the data collected from InitializeLevelData()
+    /// </summary>
+    void generateObjects()
+    {
+    
+    }
+
+
+    IEnumerator WaitForTimerRunOut()
+    {
+        yield return new WaitForSeconds(timeForLevel);
+        EndGame();
+    }
+
+
+    public void EndGame()
+    {
+        Debug.Log("Game over");
+    }
+
+
+}
---
+++ Final Project/Assets/Scripts/Levels.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// https://docs.unity3d.com/Manual/class-ScriptableObject.html
+[CreateAssetMenu(fileName = "New Level", menuName = "Level")]
+public class Levels : ScriptableObject
+{
+
+    public string levelName;
+
+    [Header("Level Goals")]
+    public int timer;
+    public List<GameObject> spawnObjects;
+    public List<GameObject> targetObjects;
+
+}
