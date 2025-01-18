dc799d5e9eafcdc6c34a90c4be192823a150049b
mecoat <66117150+mecoat@users.noreply.github.com>
2025-01-18 01:28:06 +0000

    persistent game object added

---
+++ Final Project/Assets/Scripts/MainManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//from https://learn.unity.com/tutorial/implement-data-persistence-between-scenes# accessed 18/1/24
+public class MainManager : MonoBehaviour
+{
+    public static MainManager Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
