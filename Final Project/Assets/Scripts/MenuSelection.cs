dbe7e2b9bb8442286c1295252613789b9436b325
mecoat <66117150+mecoat@users.noreply.github.com>
2025-01-18 00:23:52 +0000

    GAme loads from menu

---
+++ Final Project/Assets/Scripts/MenuSelection.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//https://www.c-sharpcorner.com/article/unity-change-scene-on-button-click-using-c-sharp-scripts-in-unity/
+using UnityEngine.SceneManagement;
+
+public class MenuSelection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        Debug.Log("loading scene");
+
+        //loads the gaame
+        SceneManager.LoadScene("GameScene");
+    }
+}
