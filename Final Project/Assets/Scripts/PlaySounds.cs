41d3356a9b5e4b363df552c36f153c9253179430
mecoat <66117150+mecoat@users.noreply.github.com>
2025-02-20 18:39:09 +0000

    basic sounds added

---
+++ Final Project/Assets/Scripts/PlaySounds.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySounds : MonoBehaviour
+{
+    //https://freesound.org/people/maxgreat/sounds/421958/ Accecssed 20/2/25
+    //game's loop by maxgreat -- https://freesound.org/s/421958/ -- License: Creative Commons 0
+    public AudioClip gameSound;
+    
+    //https://freesound.org/people/plasterbrain/sounds/397353/ Accessed 20/2/25
+    //Tada Fanfare G by plasterbrain -- https://freesound.org/s/397353/ -- License: Creative Commons 0
+    public AudioClip matchSound;
+
+    //https://freesound.org/people/BloodPixelHero/sounds/572936/ Accessed 20/2/25
+    //Error by BloodPixelHero -- https://freesound.org/s/572936/ -- License: Creative Commons 0
+    public AudioClip loseSound;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
