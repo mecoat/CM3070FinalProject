f2b5061336915e5d62edeeeb13fc43057cd322f4
mecoat <66117150+mecoat@users.noreply.github.com>
2025-02-23 21:35:23 +0000

    Instruction scene keyboard controls working

---
+++ Final Project/Assets/Scripts/InstructionsKeyListener.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class InstructionsKeyListener : MonoBehaviour
+{
+    [SerializeField]
+    Button StartGameButton;
+
+    [SerializeField]
+    Button MenuGameButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            StartGameButton.onClick.Invoke();
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            MenuGameButton.onClick.Invoke();
+        }
+    }
+}
