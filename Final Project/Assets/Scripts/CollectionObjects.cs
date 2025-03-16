7bccc40589a30c6cc46a7d01de4eaa18ac26e6f5
mecoat <66117150+mecoat@users.noreply.github.com>
2025-03-16 22:04:52 +0000

    Update CollectionObjects.cs

--- Final Project/Assets/Scripts/CollectionObjects.cs
+++ Final Project/Assets/Scripts/CollectionObjects.cs
@@ -9,36 +9,7 @@ public class CollectionObjects : MonoBehaviour
     Rigidbody rb;
 
     //bool to hold the dropping status of the object
-    //private bool isDropping = false;
-    private bool isDropping = true;  // set to true on spawn so that it counts if it lands ins the try on spawn
-
-
-    public void transferToCollector(GameObject obj)
-    {
-        //add the object to the collector (becoming a child of the collector)
-        transform.SetParent(obj.transform, true);
-        
-        //transform.localPosition = new Vector3(0f, -1.5f, 0f);
-
-        //diasble the relevant rigidbody elements so that it moves with the collector
-        rb.isKinematic = true;
-        rb.detectCollisions = false;
-    }
-
-    public void dropFromCollector()
-    {
-        //remove the object from the collector as parent
-        transform.SetParent(null);
-
-        //re-enable the rigidbody to enable it to fall from the collector
-        rb.isKinematic = false;
-        rb.detectCollisions = true;
-
-        isDropping = true;
-        //Debug.Log("object dropping = " + isDropping);
-    }
-
-
+    private bool isDropping = true;  // set to true on spawn so that it counts if it lands in the tray on spawn
 
     // Start is called before the first frame update
     void Start()
@@ -47,45 +18,64 @@ public class CollectionObjects : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    //function to transfer the parent of the object to the collector on collection
+    public void transferToCollector(GameObject obj)
     {
-        
+        //add the object to the collector (becoming a child of the collector)
+        transform.SetParent(obj.transform, true);
+
+        //diasble the relevant rigidbody elements so that it moves with the collector
+        rb.isKinematic = true;
+        rb.detectCollisions = false;
     }
 
+    //function to remove the object from the collector 
+    public void dropFromCollector()
+    {
+        //remove the object from the collector as parent
+        transform.SetParent(null);
+
+        //re-enable the rigidbody elements to enable it to fall from the collector
+        rb.isKinematic = false;
+        rb.detectCollisions = true;
+
+        //change the isDropping variable to true to indicate theat the object is dropping
+        isDropping = true;
+    }
+
+    //function that run when the object enters a trigger
     private void OnTriggerEnter(Collider trigger)
     {
-        //Debug.Log(trigger);
+        //if the object is dropping...
         if (isDropping)
         {
+            //if the trigger its the tray trigger
             if (trigger.name == "TrayTrigger")
             {
-                //Debug.Log("in the tray");
-
+                //change isDropping to false to indicate that it is no longer dropping
                 isDropping = false;
-                //Debug.Log("object dropping = " + isDropping);
 
                 //add the object into the trayObjects object
                 GameObject obj = GameObject.Find("TrayObjects");
                 transform.SetParent(obj.transform, true);
             }
+            //otherwise, if the trigger is the Arena trigger...
             else if (trigger.name == "ArenaTrigger")
             {
-                //Debug.Log("in the arena");
-
+                //change isDropping to false to indicate that it is no longer dropping
                 isDropping = false;
-                //Debug.Log("object dropping = " + isDropping);
 
                 //add the object back to the Objects object
                 GameObject obj = GameObject.Find("Objects");
                 transform.SetParent(obj.transform, true);
             }
         }
-        
     }
 
+    //function to destrooy this object
     public void destroySelf()
     {
+        //destroy this object
         Destroy(gameObject);
     }
 }
