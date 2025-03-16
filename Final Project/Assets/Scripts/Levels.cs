75b97bd53be37d09076a5b7b009b551f6563b046
mecoat <66117150+mecoat@users.noreply.github.com>
2025-03-16 13:13:01 +0000

    Update Levels.cs

--- Final Project/Assets/Scripts/Levels.cs
+++ Final Project/Assets/Scripts/Levels.cs
@@ -7,12 +7,16 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Level", menuName = "Level")]
 public class Levels : ScriptableObject
 {
-
+    //String for Level Name
     public string levelName;
 
+    //Level Goals
     [Header("Level Goals")]
+    //how much time for this level
     public int timer;
+    //which objects to spawn
     public List<GameObject> spawnObjects;
+    //which objects are targets
     public List<GameObject> targetObjects;
 
 }
