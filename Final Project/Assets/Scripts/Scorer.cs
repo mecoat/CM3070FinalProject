32f69914062100b575c8a6fc210d1e10452f5f3b
mecoat <66117150+mecoat@users.noreply.github.com>
2025-03-16 00:46:00 +0000

    scorer updated

--- Final Project/Assets/Scripts/Scorer.cs
+++ Final Project/Assets/Scripts/Scorer.cs
@@ -6,40 +6,37 @@ using UnityEngine.UI;
 
 public class Scorer : MonoBehaviour
 {
-
+    //holder for current score
     private int currScore = 0;
 
+    //the display area in the scene
     [SerializeField]
     Text scoreDisplay;
-
-    //[SerializeField]
-    //GameManager manager;
 
     // Start is called before the first frame update
     void Start()
     {
+        //run updateScore with a value of 0
         updateScpre(0);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        //updateScpre(1);
-    }
-
-
+    //function to update the score with the value input as an argument
     public void updateScpre(int addVal)
     {
+        //add the input value to the current score variable
         currScore += addVal;
         
+        //create a new string to display
         string disp = "Current Score" + "\n" + currScore.ToString();
 
+        //diplaythe string
         scoreDisplay.text = disp;
-
     }
 
+    //function to feed back the current scrore
     public int getScore()
     {
+        //returns the curent score to the requesting location
         return (currScore);
     }
 }
